eb7f13f7e16bb7b98c1993d46d57e56f8280d33f
nekacurry <anneka498@gmail.com>
2021-03-22 00:05:35 -0500

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,14 @@
 /** @type {CharData} */
 let characterData       = [];   // Initial character data set used.
 
+/** Save Data. Concatenated into array, joined into string (delimited by '|') and compressed with lz-string. */
+let timestamp = 0;        // savedata[0]      (Unix time when sorter was started, used as initial PRNG seed and in dataset selection)
+let timeTaken = 0;        // savedata[1]      (Number of ms elapsed when sorter ends, used as end-of-sort flag and in filename generation)
+let choices   = '';       // savedata[2]      (String of '0', '1' and '2' that records what sorter choices are made)
+let optStr    = '';       // savedata[3]      (String of '0' and '1' that denotes top-level option selection)
+let suboptStr = '';       // savedata[4...n]  (String of '0' and '1' that denotes nested option selection, separated by '|')
+let timeError = false;    // Shifts entire savedata array to the right by 1 and adds an empty element at savedata[0] if true.
+
 /** Intermediate sorter data. */
 let sortedIndexList = [];
 let recordDataList  = [];
@@ -101,4 +109,141 @@ function init() {
     const imageNum = e.target.options[e.target.selectedIndex].value;
     result(Number(imageNum));
   });}
-  
+
+  /** Begin sorting. */
+function start() {
+  /** Copy data into sorting array to filter. */
+  characterDataToSort = characterData.slice(0);
+
+  /** Check selected options and convert to boolean array form. */
+  optTaken = [];
+
+  options.forEach(opt => {
+    if ('sub' in opt) {
+      if (!document.getElementById(`cbgroup-${opt.key}`).checked) optTaken.push(false);
+      else {
+        const suboptArray = opt.sub.reduce((arr, val, idx) => {
+          arr.push(document.getElementById(`cb-${opt.key}-${idx}`).checked);
+          return arr;
+        }, []);
+        optTaken.push(suboptArray);
+      }
+    } else { optTaken.push(document.getElementById(`cb-${opt.key}`).checked); }
+  });
+
+  /** Convert boolean array form to string form. */
+  optStr    = '';
+  suboptStr = '';
+
+  optStr = optTaken
+    .map(val => !!val)
+    .reduce((str, val) => {
+      str += val ? '1' : '0';
+      return str;
+    }, optStr);
+  optTaken.forEach(val => {
+    if (Array.isArray(val)) {
+      suboptStr += '|';
+      suboptStr += val.reduce((str, val) => {
+        str += val ? '1' : '0';
+        return str;
+      }, '');
+    }
+  });
+
+  /** Filter out deselected nested criteria and remove selected criteria. */
+  options.forEach((opt, index) => {
+    if ('sub' in opt) {
+      if (optTaken[index]) {
+        const subArray = optTaken[index].reduce((subList, subBool, subIndex) => {
+          if (subBool) { subList.push(options[index].sub[subIndex].key); }
+          return subList;
+        }, []);
+        characterDataToSort = characterDataToSort.filter(char => {
+          if (!(opt.key in char.opts)) console.warn(`Warning: ${opt.key} not set for ${char.name}.`);
+          return opt.key in char.opts && char.opts[opt.key].some(key => subArray.includes(key));
+        });
+      }
+    } else if (optTaken[index]) {
+      characterDataToSort = characterDataToSort.filter(char => !char.opts[opt.key]);
+    }
+  });
+
+  if (characterDataToSort.length < 2) {
+    alert('Cannot sort with less than two characters. Please reselect.');
+    return;
+  }
+
+  /** Shuffle character array with timestamp seed. */
+  timestamp = timestamp || new Date().getTime();
+  if (new Date(timestamp) < new Date(currentVersion)) { timeError = true; }
+  Math.seedrandom(timestamp);
+
+  characterDataToSort = characterDataToSort
+    .map(a => [Math.random(), a])
+    .sort((a,b) => a[0] - b[0])
+    .map(a => a[1]);
+
+  /**
+   * tiedDataList will keep a record of indexes on which characters are equal (i.e. tied) 
+   * to another one. recordDataList will have an interim list of sorted elements during
+   * the mergesort process.
+   */
+
+  recordDataList  = characterDataToSort.map(() => 0);
+  tiedDataList    = characterDataToSort.map(() => -1);
+
+  /** 
+   * Put a list of indexes that we'll be sorting into sortedIndexList. These will refer back
+   * to characterDataToSort.
+   * 
+   * Begin splitting each element into little arrays and spread them out over sortedIndexList
+   * increasing its length until it become arrays of length 1 and you can't split it anymore. 
+   * 
+   * parentIndexList indicates each element's parent (i.e. where it was split from), except 
+   * for the first element, which has no parent.
+   */
+
+  sortedIndexList[0] = characterDataToSort.map((val, idx) => idx);
+  parentIndexList[0] = -1;
+
+  let midpoint = 0;   // Indicates where to split the array.
+  let marker   = 1;   // Indicates where to place our newly split array.
+
+  for (let i = 0; i < sortedIndexList.length; i++) {
+    if (sortedIndexList[i].length > 1) {
+      let parent = sortedIndexList[i];
+      midpoint = Math.ceil(parent.length / 2);
+
+      sortedIndexList[marker] = parent.slice(0, midpoint);              // Split the array in half, and put the left half into the marked index.
+      totalBattles += sortedIndexList[marker].length;                   // The result's length will add to our total number of comparisons.
+      parentIndexList[marker] = i;                                      // Record where it came from.
+      marker++;                                                         // Increment the marker to put the right half into.
+
+      sortedIndexList[marker] = parent.slice(midpoint, parent.length);  // Put the right half next to its left half.
+      totalBattles += sortedIndexList[marker].length;                   // The result's length will add to our total number of comparisons.
+      parentIndexList[marker] = i;                                      // Record where it came from.
+      marker++;                                                         // Rinse and repeat, until we get arrays of length 1. This is initialization of merge sort.
+    }
+  }
+
+  leftIndex  = sortedIndexList.length - 2;    // Start with the second last value and...
+  rightIndex = sortedIndexList.length - 1;    // the last value in the sorted list and work our way down to index 0.
+
+  leftInnerIndex  = 0;                        // Inner indexes, because we'll be comparing the left array
+  rightInnerIndex = 0;                        // to the right array, in order to merge them into one sorted array.
+
+  /** Disable all checkboxes and hide/show appropriate parts while we preload the images. */
+  document.querySelectorAll('.starting.button').forEach(el => el.style.display = 'none');
+  document.querySelector('.loading.button').style.display = 'block';
+  document.querySelector('.progress').style.display = 'block';
+  loading = true;
+
+  preloadImages().then(() => {
+    loading = false;
+    document.querySelector('.loading.button').style.display = 'none';
+    document.querySelectorAll('.sorting.button').forEach(el => el.style.display = 'block');
+    document.querySelectorAll('.sort.text').forEach(el => el.style.display = 'block');
+    display();
+  });
+}
