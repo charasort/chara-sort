afd88ad59c8d1669da0b50c5b434965f19135810
asood92 <392075+asood92@users.noreply.github.com>
2021-03-23 19:10:55 -0400

    Update and rename test.py to api_query.py

--- static/js/api_query.py
+++ static/js/api_query.py
@@ -8,7 +8,10 @@ pp = PrettyPrinter(indent=4)
 def getCharacterInfo():
     # query = request.args["search"]
     path = "https://gameofthrones.fandom.com/wikia.php?format=json&controller=MercuryApi&method=getPage&title=Category:Individuals"
+    # this is for future easy searching, works entirely just couldn't get it implemented in the frontend gracefully because of the javascript issues
+    # path = f"https://{queryname}.fandom.com/wikia.php?format=json&controller=MercuryApi&method=getPage&title=Category:Individuals"
     characterInfo = requests.get(path).json()
+    # edge case for wikis that define their character page differently
     # if characterInfo["exception"]["code"] == 404:
     #     path = "https://gameofthrones.fandom.com/wikia.php?format=json&controller=MercuryApi&method=getPage&title=Category:Characters"
     #     characterInfo = requests.get(path).json()
