edfd8a1d60a4d531377d0aba156e8b09e8f5f044
Amman Sood <392075+asood92@users.noreply.github.com>
2021-03-23 13:22:39 -0400

    testing route

---
+++ app.py
@@ -0,0 +1,25 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def getCharacterInfo():
+    # query = request.args["search"]
+    path = "https://gameofthrones.fandom.com/wikia.php?format=json&controller=MercuryApi&method=getPage&title=Category:Individuals"
+    characterInfo = requests.get(path).json()
+    characterInfo = characterInfo["data"]["nsSpecificContent"]["trendingPages"]
+    character = []
+    for i in range(7):
+        newThumb = character[i]["thumbnail"].split("revision")
+        newDict = {"title": character[i]["title"], "thumbnail": newThumb[0]}
+        character.append(newDict)
+
+    context = {"characters": character}
+
+    return render_template("sorter.html", **context)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
